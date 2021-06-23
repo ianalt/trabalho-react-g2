c56fe1bd4bac6879a29487ae2175ee9d9d2c2b6a
Ian Alt <ianalt.dev@gmail.com>
2021-06-23 15:16:21 -0300

    alteracoes na pagina de produtos

--- src/components/Produtos/Produtos.js
+++ src/components/Produtos/Produtos.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import { Component } from 'react';
+import React from "react";
+import { Component } from "react";
 
 import "bootstrap/dist/css/bootstrap.min.css";
 
@@ -11,41 +11,87 @@ import Row from "react-bootstrap/Row";
 
 import api from "../../api";
 
-export class Produtos extends Component{
-    
+export class Produtos extends Component {
     state = {
         produtos: [],
     };
 
     async componentDidMount() {
         const response = await api.get("produto");
-    
+
         this.setState({ produtos: response.data });
     }
 
-    render(){
+    render() {
         const { produtos } = this.state;
 
-        return(
-        <div>
-            <h1 style={{ textAlign: "center" }}>Produtos</h1>
-            <Container>
-            <Row>
-        {produtos.map((produto) => (
-          <Col>
-            <Card style={{ width: "18rem", height: "15rem", marginTop: "2rem" }} key={produto.id}>
-              <Card.Body>
-                <Card.Title>{produto.nome}</Card.Title>
-                <Card.Text>{produto.descricao}</Card.Text>
-                <Card.Text>R${produto.valor}</Card.Text>
-                <Button variant="primary">Comprar</Button>
-              </Card.Body>
-            </Card>
-          </Col>
-        ))}
-        </Row>
-            </Container>
-        </div>
+        // como não será possível inserir imagens,
+        // colocamos uma imagem padrão que será inserida para todos os produtos novos inserido
+        const verificaImg = (id, fotoLink) => {
+            if (id >= 10) {
+                fotoLink =
+                    "https://cptstatic.s3.amazonaws.com/imagens/enviadas/materias/materia10029/16.treinamento-garcom-cursos-cpt.jpg";
+            }
+            return fotoLink;
+        };
+
+        const verificaDesc = (descricao) => {
+            if (descricao.length > 20) {
+                return descricao.slice(0, 19) + "...";
+            } else {
+                return descricao;
+            }
+        };
+
+        return (
+            <div>
+                <h1 style={{ textAlign: "center" }}>Produtos</h1>
+                <Container>
+                    <Row>
+                        {produtos.map((produto) => (
+                            <Col>
+                                <Card
+                                    style={{
+                                        width: "18rem",
+                                        height: "auto",
+                                        marginTop: "2rem",
+                                    }}
+                                    key={produto.id}
+                                >
+                                    <Card.Body
+                                        style={{
+                                            width: "285px",
+                                            height: "auto",
+                                        }}
+                                    >
+                                        <Card.Title>{produto.nome}</Card.Title>
+                                        <Card.Text>
+                                            <img
+                                                src={verificaImg(
+                                                    produto.id,
+                                                    produto.fotoLink
+                                                )}
+                                                alt={`Foto de ${produto.nome}`}
+                                                style={{
+                                                    width: "250px",
+                                                    height: "250px",
+                                                }}
+                                            ></img>
+                                        </Card.Text>
+                                        <Card.Text>
+                                            {verificaDesc(produto.descricao)}
+                                        </Card.Text>
+                                        <Card.Text>R${produto.valor}</Card.Text>
+                                        <Button variant="primary">
+                                            Comprar
+                                        </Button>
+                                    </Card.Body>
+                                </Card>
+                            </Col>
+                        ))}
+                    </Row>
+                </Container>
+            </div>
         );
     }
 }
